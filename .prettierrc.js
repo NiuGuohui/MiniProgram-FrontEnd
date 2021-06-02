14fa02fa6df4b7266b3890782b674f3443131f57
NiuGuohui <niu15076177821@hotmail.com>
2021-06-02 13:42:05 +0800

    init: init project

---
+++ .prettierrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  // 单行输出最大长度
+  printWidth: 100,
+  // 水平缩进的空格数
+  tabWidth: 2,
+  // 使用tab而不是spaces
+  tabs: false,
+  // 在语句末尾添加回车,
+  semi: true,
+  // 使用单引号而非双引号
+  singleQuote: true,
+  // 在任何可能的多行中输入尾逗号 none(无尾逗号) es5(添加es5中被支持的尾逗号) all(在所有可能的地方都添加)
+  trailingComma: 'es5',
+  // 在对象字面量生命所使用的花括号后{和前}输出空格
+  bracketSpacing: true,
+  // 在多行jsx元素最后一行的末尾添加> 而使 >单独一行
+  jsxBracketSameLine: false,
+  // 为单行箭头函数的参数添加圆括号 avoid(尽可能不添加) always(总是添加)
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+};
